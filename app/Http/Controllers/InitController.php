939665cc9476209bf397705f1766696908b2a60d
owaisklabs <owais.klabs@gmail.com>
2022-02-14 18:52:15 +0500

    fixes

---
+++ app/Http/Controllers/InitController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Grade;
+use App\Models\Subject;
+use Illuminate\Http\Request;
+
+class InitController extends Controller
+{
+    public function getData()
+    {
+        $grade = Grade::all();
+        $subject = Subject::all();
+
+        return [
+            'garde'=>$grade,
+            'subject'=>$subject
+        ];
+    }
+}
