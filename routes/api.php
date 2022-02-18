998bacb348effc6800c3b782922976386c453db1
owaisklabs <owais.klabs@gmail.com>
2022-02-18 14:46:09 +0500

    fixes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InitController;
 use App\Http\Controllers\StudyMaterialController;
+use App\Http\Controllers\StudyNoteController;
+use App\Http\Controllers\UserActivity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->group( function () {
     Route::get('/test',[AuthController::class,'test']);
     Route::apiResource('/study-material',StudyMaterialController::class);
+    Route::apiResource('/study-notes',StudyNoteController::class);
+    Route::post('rating',[UserActivity::class,'rating']);
 });
 // Route::get('/',[AuthController::class,'test']);
 Route::get('get-data',[InitController::class,'getData']);
