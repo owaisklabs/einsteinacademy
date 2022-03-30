5e38c150f60120ff29aacce07ba03a343d7d5edb
owaisklabs <owais.klabs@gmail.com>
2022-03-31 00:12:22 +0500

    fixes

--- app/Http/Controllers/StudyNoteController.php
+++ app/Http/Controllers/StudyNoteController.php
@@ -74,7 +74,7 @@ class StudyNoteController extends Controller
         $studyNote->type  = $request->type;
         $studyNote->save();
         $user=User::find(Auth::id());
-        $user_id= Followe::where('user_id',$user->id)->pluck('follower_id');
+        $user_id= Followe::where('follower_id',$user->id)->pluck('user_id');
         $user= User::whereIn('id',$user_id)->get();
         $tokens =[];
         foreach ($user as $item){
