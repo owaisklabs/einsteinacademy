825e645fe98747c848057bcd3e0c776de5c3b02f
owaisklabs <owais.klabs@gmail.com>
2022-03-31 00:18:31 +0500

    fixes

---
+++ resources/views/website/layuot/layout.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Basic -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Darbar Caterers</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
+    <meta name="keywords"
+          content="darbar caterers and decorators, darbar, dar, bar, catring, catrers, caterings, services,decorators, food, fast food, event, managing, mangement, planner, facilities, wedding, services, food, caterers since 1955,decorator since 1955, since 1955, junaid, cats, event handling,Professional Event Manager, Professional Event Managing Agency, An Event Mangent Company Since 1955" />
+
+    <link rel="icon" type="image/png" href="img/favicon.ico">
+
+    <!-- Fonts -->
+    <link
+        href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,100italic,100,500,500italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic'
+          rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="screen">
+    <!-- Responsive -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" media="screen">
+
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="js/ie.js"></script>
+    <script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+
+</head>
+<body>
+
+</body>
+</html>
